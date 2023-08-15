f666b30d023e97076fa4f8f7f274711a9027da19
ol-deg <egoroshina@inbox.ru>
2023-08-15 13:27:42 +1000

    lesson_7

--- django/advertisment/app_advertisment/models.py
+++ django/advertisment/app_advertisment/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Advertisement(models.Model):
+    class Meta:
+        db_table = 'advertisements'
+
+    def __str__(self):
+        return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
+    title = models.CharField('Название', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
--- django/advertisment/app_lesson_4/urls.py
+++ django/advertisment/app_lesson_4/urls.py
@@ -3,8 +3,4 @@ from .views import index
 
 urlpatterns = [
     path('', index)
-<<<<<<< HEAD
 ]
-=======
-]
->>>>>>> 7edd2ffb85fb3e58aefd6cf8843ef3ffae39a359
