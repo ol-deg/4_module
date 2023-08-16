a83682c63c3fb3506d4fed172a4f6b08349310a4
ol-deg <egoroshina@inbox.ru>
2023-08-16 13:51:36 +1000

    lesson_8

--- django/advertisment/app_advertisment/models.py
+++ django/advertisment/app_advertisment/models.py
@@ -1,11 +1,32 @@
 from django.db import models
+from django.contrib import admin
+from django.utils.html import format_html
+from django.utils import timezone
 
 class Advertisement(models.Model):
     class Meta:
         db_table = 'advertisements'
+    @admin.display(description='Дата создания')
+    def created_date(self):
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: green; font-weight: bold;">Сегодня в {} </span>', created_time
+            )
+        return self.created_at.strftime('%d.%m.%Y в %H:%M:%S')
+
+    @admin.display(description='Дата последнего обновления')
+    def updated_date(self):
+        if self.update_at.date() == timezone.now().date():
+            created_time = self.update_at.time().strftime('%H:%M:%S')
+            return format_html(
+                '<span style="color: blue; font-weight: bold;">Сегодня в {} </span>', created_time
+            )
+        return self.update_at.strftime('%d.%m.%Y в %H:%M:%S')
 
     def __str__(self):
         return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
+
     title = models.CharField('Название', max_length=128)
     description = models.TextField('Описание')
     price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
