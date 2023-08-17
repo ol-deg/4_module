fb32b645760a0884c6cba5542561ae1e36dbf4d2
ol-deg <egoroshina@inbox.ru>
2023-08-17 18:05:35 +1000

    lesson_9

--- django/advertisment/app_advertisment/admin.py
+++ django/advertisment/app_advertisment/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Advertisement
 
 class AdvertisementAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date','auction']
+    list_display = ['id', 'title', 'description', 'price', 'created_date', 'updated_date','auction', 'get_html_image']
     list_filter = ['created_at', 'auction']
     actions = ['make_auction_as_false', 'make_auction_as_true']
 
--- django/advertisment/app_advertisment/models.py
+++ django/advertisment/app_advertisment/models.py
@@ -2,8 +2,19 @@ from django.db import models
 from django.contrib import admin
 from django.utils.html import format_html
 from django.utils import timezone
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class Advertisement(models.Model):
+    title = models.CharField('Название', max_length=128)
+    description = models.TextField('Описание')
+    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
+    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+    image = models.ImageField('Изображение', upload_to='advertisements/', null=True, blank=True)
     class Meta:
         db_table = 'advertisements'
     @admin.display(description='Дата создания')
@@ -27,9 +38,10 @@ class Advertisement(models.Model):
     def __str__(self):
         return f'<Advertisement: Advertisement(id={self.id}, title={self.title}, price={self.price})>'
 
-    title = models.CharField('Название', max_length=128)
-    description = models.TextField('Описание')
-    price = models.DecimalField('Цена', max_digits=10, decimal_places=2)
-    auction = models.BooleanField('Торг', help_text='Отметьте, если торг уместен')
-    created_at = models.DateTimeField(auto_now_add=True)
-    update_at = models.DateTimeField(auto_now=True)
+    @admin.display(description='Картинка')
+    def get_html_image(self):
+        if self.image:
+            return format_html(
+                '<img src="{url}" style="max-width: 80px; max-height: 80px;"', url=self.image.url
+            )
+
